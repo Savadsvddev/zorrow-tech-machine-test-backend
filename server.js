8132536def6c4e813ee1604755b1a27fbd479ca5
savad <savadsvd088@gmail.com>
2026-03-16 16:02:49 +0530

    committed

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(
+  cors({
+    origin: "https://machine-test-46677.web.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+  })
+);
+
 connectDB();
 
 app.use("/user", userRoutes);
