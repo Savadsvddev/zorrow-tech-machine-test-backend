d6ec70e5fd8a9e10c28de1ed76b1995c77e6d9f6
savad <savadsvd088@gmail.com>
2026-03-17 10:51:49 +0530

    committed

--- routes/attendance.js
+++ routes/attendance.js
@@ -1,11 +1,53 @@
 const express = require("express");
 const router = express.Router();
 const attendance = require("../models/attendanceModel");
-// const authMiddleware = require("../middleware/auth");
+const authMiddleware = require("../middleware/auth");
 const fs = require("fs");
 const path = require("path");
 
-router.post("/checkin", async (req, res) => {
+router.post("/checkin", authMiddleware, async (req, res) => {
+  try {
+    const { user_id, latitude, longitude, image } = req.body;
+
+    if (!user_id || !latitude || !longitude || !image) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required"
+      });
+    }
+
+   
+    const base64Data = image.replace(/^data:image\/jpeg;base64,/, "");
+    const fileName = Date.now() + "-image.jpg";
+    const filePath = path.join("uploads", fileName);
+
+    fs.writeFileSync(filePath, base64Data, "base64");
+
+    const attendanceRecord = new attendance({
+      user_id,
+      latitude,
+      longitude,
+      image: filePath,
+      timestamp: new Date()
+    });
+
+    const savedAttendance = await attendanceRecord.save();
+
+    res.status(201).json({
+      success: true,
+      message: "Check-in successful",
+      data: savedAttendance
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
+});
+
+router.post("/checkout", authMiddleware,async (req, res) => {
   try {
     const { user_id, latitude, longitude, image } = req.body;
 
--- server.js
+++ server.js
@@ -2,30 +2,31 @@ const express = require("express");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/user");
 const attendanceRoutes = require("./routes/attendance");
+const cors = require("cors");
 const app = express();
 
-// Simple CORS middleware
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  
-  if (req.method === 'OPTIONS') {
-    res.sendStatus(200);
-  } else {
-    next();
-  }
-});
 
+app.use(cors());
 app.use(express.json());
+
+
 
 connectDB();
 
+
+
 app.use("/user", userRoutes);
+
 app.use("/attendance", attendanceRoutes);
+
+
 
 const PORT = 5000;
 
+
+
 app.listen(PORT, () => {
+
   console.log(`Server running on port ${PORT}`);
+
 });
