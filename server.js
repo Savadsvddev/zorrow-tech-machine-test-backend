51aee2a1b9a3b7438f911b12c35d2b98f0be11cb
savad <savadsvd088@gmail.com>
2026-03-18 12:59:45 +0530

    comitted

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/user", userRoutes);
 app.use("/attendance", attendanceRoutes);
 
-const PORT = process.env.PORT ||5000;
+const PORT = process.env.PORT ;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
